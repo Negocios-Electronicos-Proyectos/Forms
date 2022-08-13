a0f62b2208eb33fe33be5af423351944878e4276
Adelso <adelso.guerra@outlook.com>
2022-08-13 02:58:12 -0600

    Actualización de cambios de nav y footer

--- index.py
+++ index.py
@@ -1,26 +1,32 @@
+# Creator: Adelso Guerra
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+#Inicio
 @app.route('/')
 def home():
     return render_template("home.html")
 
-
+#Acerca De
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-
+#Noticias
 @app.route('/news')
 def news():
     return render_template("news.html")
 
+#Ventas
+@app.route('/sales')
+def sales():
+    return render_template("sales.html")
 
-@app.route('/ventas')
-def ventas():
-    return render_template("ventas.html")
+#Entretenimiento
+@app.route('/entertainment')
+def entertainment():
+    return render_template("entertainment.html")
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
