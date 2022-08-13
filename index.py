e4bf0e70654c10cada5e8f3fe51789b34081fba5
Adelso <adelso.guerra@outlook.com>
2022-08-13 15:03:58 -0600

    Eliminación de archivos basura

--- index.py
+++ index.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Inicio
-
-
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -16,29 +14,21 @@ def home2():
     return render_template("home.html")
 
 # Acerca De
-
-
 @app.route('/about')
 def about():
     return render_template("about.html")
 
 # Noticias
-
-
 @app.route('/news')
 def news():
     return render_template("news.html")
 
 # Ventas
-
-
 @app.route('/sales')
 def sales():
     return render_template("sales.html")
 
 # Entretenimiento
-
-
 @app.route('/entertainment')
 def entertainment():
     return render_template("entertainment.html")
@@ -55,5 +45,5 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
